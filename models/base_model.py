dcda1d32405dfab451ff947b1314118fecf29350
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2023-08-24 01:00:35 +0300

    chore: update the base model

--- models/base_model.py
+++ models/base_model.py
@@ -1,103 +1,96 @@
 #!/usr/bin/python3
+"""This is the base model class for AirBnB"""
 
-"""
-Module for the BaseModel class.
-
-This module defines the BaseModel class, which is a
-base class that defines common attributes and methods for
-other classes.
-
-"""
-
-from models import storage
 import uuid
+import models
 from datetime import datetime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, DateTime
+
+Base = declarative_base()
 
 
 class BaseModel:
     """
-    A base class that defines common attributes and methods for other classes.
-
-    Public instance attributes:
-    - id: string - A unique identifier assigned using uuid when an instance is
-    created.
-    - created_at: datetime - The datetime when an instance is created.
-    - updated_at: datetime - The datetime when an instance is last updated.
-
-    Public instance methods:
-    - save(self): Updates the public instance attribute updated_at with the
-    current datetime and calls the save() method of storage.
-    - to_dict(self): Returns a dictionary representation of the BaseModel
-    instance.
-
+    This class defines all common attributes/methods
+    for other classes
+    Attributes:
+        id: primary key, string of 60 chars
+        created_at: datetime obj, indicate when the instance is created
+        updated_at: datetime obj, indicate when the instance is updated
     """
+    id = Column(String(60), primary_key=True)
+    created_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
+    updated_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
 
     def __init__(self, *args, **kwargs):
         """
-        Initializes a new instance of the BaseModel class.
-
-        If kwargs is not empty, it recreates an instance using the dictionary
-        representation. Otherwise, it creates a new instance with a new id
-        and created_at.
-
+        Instantiation of base model class
         Args:
-            *args: Not used.
-            **kwargs: A dictionary representing the instance attributes.
-
+            args: it won't be used
+            kwargs: arguments for the constructor of the BaseModel
+        Attributes:
+            id: unique id generated
+            created_at: creation date
+            updated_at: updated date
         """
         if kwargs:
-            if 'id' not in kwargs:
-                kwargs['id'] = str(uuid.uuid4())
-            if 'created_at' in kwargs:
-                kwargs['created_at'] = datetime.strptime(
-                    kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
-            if 'updated_at' in kwargs:
-                kwargs['updated_at'] = datetime.strptime(
-                    kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
             for key, value in kwargs.items():
-                if key != '__class__':
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
-            storage.new(self)
+            self.created_at = self.updated_at = datetime.now()
+        if not self.id:
+            self.id = str(uuid.uuid4())
+        date = datetime.now()
+        if not self.created_at:
+            self.created_at = self.updated_at = date
+        if not self.updated_at:
+            self.updated_at = date
 
     def __str__(self):
         """
-        Returns a string representation of the BaseModel instance.
-
-        The string includes the class name, id, and instance attributes.
-
-        Returns:
-            str: A string representation of the BaseModel instance.
-
+        Returns a string representation of an object
+        Return:
+            returns a string of class name, id, and dictionary
         """
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        return "[{}] ({}) {}".format(
+            type(self).__name__, self.id, self.__dict__)
+
+    def __repr__(self):
+        """
+        Return a string representaion of an object instantiation
+        """
+        return self.__str__()
 
     def save(self):
         """
-        Updates the public instance attribute updated_at with the current
-        datetime and calls the save() method of storage.
-
+        Updates the public instance attribute updated_at to current
         """
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.new(self)
+        models.storage.save()
 
     def to_dict(self):
         """
-        Returns a dictionary representation of the BaseModel instance.
-
-        The dictionary contains all instance attributes, including the
-        class name, id, created_at, and updated_at attributes.
-
-        Returns:
-            dict: A dictionary representation of the BaseModel instance.
-
+        Creates dictionary of the class and returns an object dict
+        Return:
+            returns a dictionary of all the key values in __dict__
         """
-        obj_dict = self.__dict__.copy()
-        obj_dict['__class__'] = self.__class__.__name__
-        obj_dict['created_at'] = self.created_at.isoformat()
-        obj_dict['updated_at'] = self.updated_at.isoformat()
+        obj_dict = dict(self.__dict__)
+        obj_dict["__class__"] = str(type(self).__name__)
+        obj_dict["created_at"] = self.created_at.isoformat()
+        obj_dict["updated_at"] = self.updated_at.isoformat()
+        if obj_dict.get('_sa_instance_state'):
+            del obj_dict['_sa_instance_state']
         return obj_dict
-    
+
+    def delete(self):
+        """
+        Deletes the current instance from the storage
+        using file storage instance method delete
+        """
+        models.storage.delete(self)
