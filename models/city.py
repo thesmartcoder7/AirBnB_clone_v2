d4925fca7fca259633a5ebd37f4f3d7828c023ca
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2023-08-24 00:52:47 +0300

    chore: update projet parts

--- models/city.py
+++ models/city.py
@@ -1,9 +1,22 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+
+from models.base_model import BaseModel, Base
+from models.state import State
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class City(BaseModel):
-    """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+class City(BaseModel, Base):
+    """
+    This is the class for City
+    Attributes:
+        __tablename__: name of the table represented
+        state_id: The state id
+        name: input name
+    """
+
+    __tablename__ = "cities"
+    state_id = Column(String(60), ForeignKey(State.id), nullable=False)
+    name = Column(String(128), nullable=False)
+    places = relationship("Place", cascade="all, delete", backref="city")
--- models/state.py
+++ models/state.py
@@ -1,8 +1,33 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from os import environ as env
+import models
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """
+    This is the class for State
+    Attributes:
+        __tablename__: table name
+        name: input name
+        cities: relation to cities table
+    """
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade="all, delete", backref="state")
+
+    if env.get('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def cities(self):
+            """get all cities with the current state id
+            from filestorage
+            """
+            list = [
+                value for key, value in models.storage.all(models.City).items()
+                if value.state_id == self.id
+            ]
+            return (list)
