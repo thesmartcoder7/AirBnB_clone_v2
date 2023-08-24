c9f8f46f4a4907a05cab3465d31ce4cf447123a1
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2023-08-24 03:41:19 +0300

    chore: update console's create method - test

--- console.py
+++ console.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Console Module """
 import cmd
+import re
 import sys
 from models.base_model import BaseModel
 from models.__init__ import storage
@@ -10,6 +11,16 @@ from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.review import Review
+
+
+def convert_to_proper_type(value):
+    if re.match(r'^".*"$', value):
+        return value.replace("_", " ").replace("\"", "")
+    elif "." in value:
+        return float(value)
+    elif re.match(r"\d+", value):
+        return int(value)
+    return value
 
 
 class HBNBCommand(cmd.Cmd):
@@ -73,7 +84,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] is '{' and pline[-1] is '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -113,29 +124,48 @@ class HBNBCommand(cmd.Cmd):
         """ Overrides the emptyline method of CMD """
         pass
 
+    """    def do_create(self, args):
+    # Create an object of any class
+    if not args:
+        print("** class name missing **")
+        return
+    elif args not in HBNBCommand.classes:
+        print("** class doesn't exist **")
+        return
+    new_instance = HBNBCommand.classes[args]()
+    storage.save()
+    print(new_instance.id)
+    storage.save()
+    """
     def do_create(self, args):
-        """ Create an object of any class"""
-        try:
-            if not args:
-                raise SyntaxError()
-            split1 = args.split(' ')
-            new_instance = eval('{}()'.format(split1[0]))
-            params = split1[1:]
-            for param in params:
-                k, v = param.split('=')
-                try:
-                    attribute = HBNBCommand.verify_attribute(v)
-                except:
-                    continue
-                if not attribute:
-                    continue
-                setattr(new_instance, k, attribute)
-            new_instance.save()
-            print(new_instance.id)
-        except SyntaxError:
+        '''
+        Create a new instance of class BaseModel and save it
+        to the JSON file.
+        '''
+        if len(args) == 0:
             print("** class name missing **")
-        except NameError as e:
+            return
+
+        class_name, *attr_args = args.split()
+
+        if class_name not in HBNBCommand.classes:
             print("** class doesn't exist **")
+            return
+
+        new_instance = HBNBCommand.classes[class_name]()
+
+        for arg in attr_args:
+            key, value = arg.split('=')
+
+            try:
+                setattr(new_instance, key, convert_to_proper_type(value))
+            except AttributeError:
+                print(
+                    f"Attribute '{key}' doesn't exist in class '{class_name}'"
+                    )
+
+        new_instance.save()
+        print(new_instance.id)
 
     def help_create(self):
         """ Help information for the create method """
@@ -198,7 +228,7 @@ class HBNBCommand(cmd.Cmd):
         key = c_name + "." + c_id
 
         try:
-            del(storage.all()[key])
+            del (storage.all()[key])
             storage.save()
         except KeyError:
             print("** no instance found **")
@@ -330,6 +360,7 @@ class HBNBCommand(cmd.Cmd):
         """ Help information for the update class """
         print("Updates an object with new information")
         print("Usage: update <className> <id> <attName> <attVal>\n")
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
