82790848339a3c627abec899c0d2df6be37fd889
thesmartcoder7 <samuel.martins4.sm@gmail.com>
2023-09-24 17:39:58 +0300

    chore: update the states and state file

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,34 +1,63 @@
 #!/usr/bin/python3
-""""Create a web app to display states as a list"""
-from models import storage
+"""This script starts a Flask web application."""
+
+# Import necessary modules and classes
 from flask import Flask, render_template
+from models import storage
+from models.state import State
+import subprocess
 
-
+# Create a Flask web application instance
 app = Flask(__name__)
 
 
-@app.route("/states", strict_slashes=False)
+@app.route('/states', strict_slashes=False)
 def states():
-    """Get all states in database"""
-    states = storage.all("State")
-    return render_template("9-states.html", state=states)
+    """Render a list of states.
+
+    This function is executed when a request to
+    0.0.0.0:/5000/states is made.
+
+    Returns:
+        str: HTML template rendered with a list of states.
+    """
+    state_list = storage.all(State)
+    states = []
+    for value in state_list.values():
+        states.append(value)
+    return render_template('9-states.html', states=states, id=None)
 
 
-@app.route("/states/<id>", strict_slashes=False)
-def states_id(id):
-    """Get all states in database"""
-    states = storage.all("State").values()
-    for state in states:
-        if state.id == id:
-            return render_template("9-states.html", state=state)
-    return render_template("9-states.html")
+@app.route('/states/<id>', strict_slashes=False)
+def single_state(id):
+    """Render information about a single state.
+
+    This function is executed when a request to
+    0.0.0.0:/5000/states/<id> is made.
+
+    Args:
+        id (str): The ID of the state to display information about.
+
+    Returns:
+        str: HTML template rendered with information about
+        the specified state.
+    """
+    state_list = storage.all(State)
+    state = {}
+    for key, value in state_list.items():
+        if value.id == id:
+            state = state_list[key]
+    return render_template('9-states.html', id=id, state=state)
 
 
 @app.teardown_appcontext
-def teardown(exc):
-    """Close sqlalchemy session"""
+def tear_down_context(exception):
+    """Remove the current SQLAlchemy Session when the app
+    context is torn down."""
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
+    subprocess.run(["export", "FLASK_APP=9-states.py"])
+    subprocess.run(["flask", "run"])
